a1a2bb81ce606fdf7c3f27198404d94e85f49b44
QenSel <125572813+QenSel@users.noreply.github.com>
2023-10-16 14:25:57 +0200

    Initial commit

---
+++ PeopleManager.Core/PeopleManagerDbContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PeopleManager.Model;
+
+namespace PeopleManager.Core
+{
+    public class PeopleManagerDbContext : IdentityDbContext
+    {
+
+        public PeopleManagerDbContext(DbContextOptions<PeopleManagerDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People => Set<Person>();
+        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>()
+                .HasOne(v => v.ResponsiblePerson)
+                .WithMany(p => p.ResponsibleForVehicles)
+                .HasForeignKey(v => v.ResponsiblePersonId)
+                .IsRequired(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void Seed()
+        {
+            var bavoUserEmail = "bavo.ketels@vives.be";
+            var bavoUser = new IdentityUser
+            {
+                Email = bavoUserEmail,
+                NormalizedEmail = bavoUserEmail.ToUpper(),
+                UserName = bavoUserEmail,
+                NormalizedUserName = bavoUserEmail.ToUpper(),
+                ConcurrencyStamp = "9d898cd7-2227-44ed-8fb2-3468d2145aae",
+                SecurityStamp = "N7URV5X4PI2IFC3I32AVDCDC4Y7XSMAQ",
+                PasswordHash =
+                    "AQAAAAIAAYagAAAAEL5uZw6smTCysavHMSzc4In30igjGfcG7B6b3rFgDpiTl33+D7dbEjsEevWd2a6rIQ==", //Test123$
+            };
+            Users.Add(bavoUser);
+
+            var bavoPerson = new Person
+            {
+                FirstName = "Bavo",
+                LastName = "Ketels",
+                Email = "bavo.ketels@vives.be",
+                Description = "Lector"
+            };
+            var wimPerson = new Person
+            {
+                FirstName = "Wim",
+                LastName = "Engelen",
+                Email = "wim.engelen@vives.be",
+                Description = "Opleidingshoofd"
+            };
+
+            People.AddRange(new List<Person>
+            {
+                bavoPerson,
+                new Person{FirstName = "Isabelle", LastName = "Vandoorne", Email = "isabelle.vandoorne@vives.be" },
+                wimPerson,
+                new Person{FirstName = "Ebe", LastName = "Deketelaere", Email = "ebe.deketelaere@vives.be" }
+            });
+
+            Vehicles.AddRange(new[] {
+                new Vehicle{LicensePlate = "1-ABC-123", ResponsiblePerson = bavoPerson},
+                new Vehicle{LicensePlate = "THE_BOSS", Brand= "Ferrari", Type="448", ResponsiblePerson = wimPerson},
+                new Vehicle{LicensePlate = "SALES_GUY_1", Brand= "Audi", Type="e-tron", ResponsiblePerson = bavoPerson},
+                new Vehicle{LicensePlate = "DESK_1", Brand= "Fiat", Type="Punto", ResponsiblePerson = bavoPerson}});
+
+            SaveChanges();
+        }
+    }
+}
---
+++ PeopleManager.Ui.Mvc/ApiServices/VehicleApiService.cs
@@ -0,0 +1,90 @@
+﻿using PeopleManager.Dto.Requests;
+using PeopleManager.Dto.Results;
+using PeopleManager.Ui.Mvc.ApiServices.Extensions;
+using Vives.Services.Model;
+
+namespace PeopleManager.Ui.Mvc.ApiServices
+{
+    public class VehicleApiService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public VehicleApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IList<VehicleResult>> Find()
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            
+            var route = "/api/vehicles";
+            var httpResponse = await httpClient.GetAsync(route);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var vehicles = await httpResponse.Content.ReadFromJsonAsync<IList<VehicleResult>>();
+
+            return vehicles ?? new List<VehicleResult>();
+        }
+
+        public async Task<VehicleResult?> Get(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            var route = $"/api/vehicles/{id}";
+            var httpResponse = await httpClient.GetAsync(route);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            return await httpResponse.Content.ReadFromJsonAsync<VehicleResult>();
+        }
+
+        public async Task<ServiceResult<VehicleResult?>> Create(VehicleRequest vehicle)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            var route = "/api/vehicles";
+            var httpResponse = await httpClient.PostAsJsonAsync(route, vehicle);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var serviceResult = await httpResponse.Content.ReadFromJsonAsync<ServiceResult<VehicleResult?>>();
+            if (serviceResult is null)
+            {
+                return new ServiceResult<VehicleResult?>().ApiError();
+            }
+            return serviceResult;
+        }
+
+        public async Task<ServiceResult<VehicleResult?>> Update(int id, VehicleRequest vehicle)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            var route = $"/api/vehicles/{id}";
+            var httpResponse = await httpClient.PutAsJsonAsync(route, vehicle);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var serviceResult = await httpResponse.Content.ReadFromJsonAsync<ServiceResult<VehicleResult?>>();
+            if (serviceResult is null)
+            {
+                return new ServiceResult<VehicleResult?>().ApiError();
+            }
+            return serviceResult;
+        }
+
+        public async Task<ServiceResult> Delete(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            var route = $"/api/vehicles/{id}";
+            var httpResponse = await httpClient.DeleteAsync(route);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var serviceResult = await httpResponse.Content.ReadFromJsonAsync<ServiceResult>();
+            if (serviceResult is null)
+            {
+                return new ServiceResult().ApiError();
+            }
+            return serviceResult;
+        }
+    }
+}
